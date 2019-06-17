db64b6fe9a7e638f3d91baa3508b23e859f7824d
jayleetx <jaylee@reed.edu>
2019-06-17 14:25:09 -0700

    Add initial tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(cps)
+
+test_check("cps")
---
+++ tests/testthat/test-year_scripts.R
@@ -0,0 +1,14 @@
+for (year in seq(2010, 2018, 2)) {
+  # each file should have a unique "year of interview" field that corresponds to the survey year
+  test_that(paste(year, "CPS_YEAR is unique and correct"), {
+    expect_equal(unique(get(paste0('cpsvrs', year, '_direct'))$CPS_YEAR), year)
+  })
+
+  # if VRS_VOTE is out of universe on a respondent, all of the VRS questions should be OOU
+  test_that(paste(year, "VRS_VOTE out of universe is always OOU"), {
+    not_in_univ <- filter(get(paste0('cpsvrs', year, '_direct')), VRS_VOTE == -1) %>%
+      select(starts_with("VRS_"))
+    expect_true(all(not_in_univ == -1))
+  })
+}
+
