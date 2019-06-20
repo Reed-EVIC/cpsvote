5a01886cb4f72a1dc7139c692243b904e9f27b45
jayleetx <jaylee@reed.edu>
2019-06-20 13:38:30 -0700

    Change method because the data changed

--- tmp/make_plot.R
+++ tmp/make_plot.R
@@ -11,16 +11,11 @@ cpsvrs_weighted <- as_survey_design(cpsvrs, weights = WEIGHT)
 
 threevote <- cpsvrs_weighted %>%
   mutate(CPS_YEAR = factor(CPS_YEAR, ordered = TRUE)) %>%
-  mutate(vote_method = case_when(
-    VRS_VBM == "By mail" ~ "Mail",
-    VRS_VBM == "In person" & VRS_ELEXDAY == "Before election day" ~ "Early",
-    VRS_VBM == "In person" & VRS_ELEXDAY == "On election day" ~ "Election day"
-    )) %>%
-  filter(!is.na(vote_method)) %>%
+  filter(!is.na(VRS_VOTEMETHOD_COLLAPSE)) %>%
   group_by(CPS_YEAR, CPS_STATE) %>%
-  summarize(`Election day` = survey_mean(vote_method == 'Election day'),
-            Mail = survey_mean(vote_method == 'Mail'),
-            Early = survey_mean(vote_method == 'Early')) %>%
+  summarize(`Election day` = survey_mean(VRS_VOTEMETHOD_COLLAPSE == 'Election day'),
+            Mail = survey_mean(VRS_VOTEMETHOD_COLLAPSE == 'Mail'),
+            Early = survey_mean(VRS_VOTEMETHOD_COLLAPSE == 'Early')) %>%
   select(-ends_with('_se')) %>%
   mutate(check = `Election day` + Mail + Early)
 
