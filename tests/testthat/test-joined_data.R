ea3d93ddca031118f29169ffe01a90054a23ac29
jayleetx <jaylee@reed.edu>
2019-08-20 12:13:13 -0700

    CHanges, probably will get dropped

--- tests/testthat/test-joined_data.R
+++ tests/testthat/test-joined_data.R
@@ -1,3 +1,7 @@
+test_that("years are in range", {
+  expect_true(all(cpsvrs$CPS_YEAR %in% 1994:2018))
+})
+
 test_that("states joined", {
   expect_setequal(cpsvrs$CPS_STATE, c(state.abb, "DC"))
 })
@@ -14,24 +18,8 @@ test_that("no zero-weights", {
   expect_gt(min(cpsvrs$WEIGHT), 0)
 })
 
-test_that('race is refactored fully', {
-  expect_setequal(unique(cpsvrs_bound$CPS_RACE), c(NA, unique_race))
-})
-
-test_that('hispanic marker is refactored fully', {
-  expect_setequal(unique(cpsvrs_bound$CPS_HISP), unique_hisp)
-})
-
-test_that('residence is refactored fully', {
-  expect_setequal(unique(cpsvrs_bound$VRS_RESIDENCE), c(NA, unique_residence))
-})
-
-test_that('reason for not voting is refactored fully', {
-  expect_setequal(unique(cpsvrs_bound$VRS_NOVOTEWHY), c(NA, unique_novote))
-})
-
-test_that('registration method is refactored fully', {
-  expect_setequal(unique(cpsvrs_bound$VRS_REGHOW), c(NA, unique_reghow))
+test_that('factors were refactored fully', {
+  expect_false(any(cpsvrs == "Other", na.rm = TRUE))
 })
 
 test_that('select caught all of the columns', {
