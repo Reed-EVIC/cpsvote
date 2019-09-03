12c0f3b5e907f0c586cced3ff1760b4bc75c4438
jayleetx <jaylee@reed.edu>
2019-09-03 10:42:56 -0700

    Add pipe, slight edit and export download functions

--- R/file_downloads.R
+++ R/file_downloads.R
@@ -12,19 +12,16 @@
 #' years (since 1964), so any entry in `years` outside of this will be skipped.
 #' * Currently the package only supports downloads from 1994 onwards, so any 
 #' entry in `years` before 1994 will be skipped.
+#' @export
 download_docs <- function(path, years = seq(1994, 2018, 2), overwrite = FALSE) {
+  
+  # sanitize inputs #####
   
   # years must be numeric
   if (!is.numeric(years)) stop('Argument "years" must be numeric')
   
   # and also not have NAs
   years <- years[!is.na(years)]
-  
-  # if they're all NA, stop
-  if (length(years) == 0) {
-    message("No files downloaded")
-    return()
-  } 
   
   # years must be from 1994 onwards
   if (any(years < 1994)) {
@@ -55,13 +52,6 @@ download_docs <- function(path, years = seq(1994, 2018, 2), overwrite = FALSE) {
     years <- years[years %in% seq(1964, 2018, 2)]
   }
   
-  # create file paths to save docs to
-  # controls for if there's a slash at the end or not
-  dir.create(path, showWarnings = FALSE)
-  file_names <- paste0(path, 
-                       ifelse(stringr::str_detect(path, "/$"), "", "/"), 
-                       paste0("cps_nov", years, ".pdf"))
-  
   # overwrite must be T/F
   if (!(overwrite %in% c(T, F))) {
     warning("Argument 'overwrite' must be TRUE or FALSE; defaulting to FALSE",
@@ -69,11 +59,25 @@ download_docs <- function(path, years = seq(1994, 2018, 2), overwrite = FALSE) {
     overwrite <- FALSE
   }
   
+  # actually download the relevant files #####
+  # create file paths to save docs to
+  # controls for if there's a slash at the end or not
+  dir.create(path, showWarnings = FALSE)
+  file_names <- paste0(path, 
+                       ifelse(stringr::str_detect(path, "/$"), "", "/"), 
+                       paste0("cps_nov", years, ".pdf"))
+  
   # remove years / file names that already exist, if overwrite is FALSE
   if (!overwrite) {
     years <- years[!file.exists(file_names)]
     file_names <- file_names[!file.exists(file_names)]
   }
+  
+  # if they're all gone, stop
+  if (length(years) == 0) {
+    message("No files downloaded")
+    return()
+  } 
   
   url_names <- dplyr::case_when(
     years < 2011 ~ paste0("data.nber.org/cps/cpsnov", stringr::str_sub(years, 3, 4), ".pdf"),
@@ -103,19 +107,16 @@ download_docs <- function(path, years = seq(1994, 2018, 2), overwrite = FALSE) {
 #' years (since 1964), so any entry in `years` outside of this will be skipped.
 #' * Currently the package only supports downloads from 1994 onwards, so any 
 #' entry in `years` before 1994 will be skipped.
+#' @export
 download_data <- function(path, years = seq(1994, 2018, 2), overwrite = FALSE) {
+  
+  # sanitize inputs #####
   
   # years must be numeric
   if (!is.numeric(years)) stop('Argument "years" must be numeric')
   
   # and also not have NAs
   years <- years[!is.na(years)]
-  
-  # if they're all NA, stop
-  if (length(years) == 0) {
-    message("No files downloaded")
-    return()
-  } 
   
   # years must be from 1994 onwards
   if (any(years < 1994)) {
@@ -146,13 +147,6 @@ download_data <- function(path, years = seq(1994, 2018, 2), overwrite = FALSE) {
     years <- years[years %in% seq(1964, 2018, 2)]
   }
   
-  # create file paths to save data to
-  # controls for if there's a slash at the end or not
-  dir.create(path, showWarnings = FALSE)
-  file_names <- paste0(path, 
-                       ifelse(stringr::str_detect(path, "/$"), "", "/"), 
-                       paste0("cps_nov", years, ".zip"))
-  
   # overwrite must be T/F
   if (!(overwrite %in% c(T, F))) {
     warning("Argument 'overwrite' must be TRUE or FALSE; defaulting to FALSE",
@@ -160,11 +154,26 @@ download_data <- function(path, years = seq(1994, 2018, 2), overwrite = FALSE) {
     overwrite <- FALSE
   }
   
+  # actually download the relevant files #####
+  
+  # create file paths to save data to
+  # controls for if there's a slash at the end or not
+  dir.create(path, showWarnings = FALSE)
+  file_names <- paste0(path, 
+                       ifelse(stringr::str_detect(path, "/$"), "", "/"), 
+                       paste0("cps_nov", years, ".zip"))
+  
   # remove years / file names that already exist, if overwrite is FALSE
   if (!overwrite) {
     years <- years[!file.exists(file_names)]
     file_names <- file_names[!file.exists(file_names)]
   }
+  
+  # if they're all gone, stop
+  if (length(years) == 0) {
+    message("No files downloaded")
+    return()
+  } 
   
   url_names <- dplyr::case_when(
     years < 2011 ~ paste0("data.nber.org/cps/cpsnov", stringr::str_sub(years, 3, 4), ".zip"),
--- data-raw/save_catalog.R
+++ data-raw/save_catalog.R
@@ -3,12 +3,16 @@ catalog <- readxl::read_excel(here::here('data-raw', 'catalog.xlsx'), sheet = 1)
 years <- unique(catalog$year)
 
 read_year_sheet <- function(year) {
-  readxl::read_excel(here('docs', 'catalog.xlsx'), sheet = as.character(year))
+  readxl::read_excel(here::here('data-raw', 'catalog.xlsx'), 
+                     sheet = as.character(year), 
+                     col_types = "text")
 }
 
-factoring <- lapply(years, read_year_sheet)
+factoring <- dplyr::bind_rows(lapply(years, read_year_sheet))
 
-fwf_key <- append(factoring, list(catalog), after = 0)
-names(fwf_key) <- c("catalog", years)
+fwf_key <- append(list(factoring), list(catalog), after = 0)
+names(fwf_key) <- c("columns", "factoring")
 
-usethis::use_data(fwf_key, internal = TRUE)
+usethis::use_data(fwf_key, internal = TRUE, overwrite = TRUE)
+
+devtools::document()
