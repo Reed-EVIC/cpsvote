e699bb8e0b22a769be89bd695ac73e13aa85721e
jayleetx <jaylee@reed.edu>
2019-11-14 15:00:29 -0800

    Add reweight function to R/

--- sandbox/cps_reweight.R
+++
@@ -1,8 +0,0 @@
-# this needs documentation
-# also maybe it should go into the canonical cps data included
-cps_reweight <- function(data) {
-  thing <- dplyr::left_join(data, reweight,
-                            by = c("CPS_YEAR", "CPS_STATE", "VRS_VOTE")) %>%
-    dplyr::select(-cps_prop, -vep_prop) %>%
-    dplyr::mutate(WEIGHT_TURNOUT = WEIGHT * REWEIGHT)
-}
