94187b9cb5b11e54c094bbd8c52d16e2740e455c
jayleetx <jay.lee.tx@gmail.com>
2020-03-12 10:36:06 -0700

    Initial commit of re-factoring function

---
+++ sandbox/factor_cps.R
@@ -0,0 +1,13 @@
+#' Apply factor levels to raw CPS data
+#' 
+#' @description The CPS publishes their data in a numeric format, with a separate 
+#' PDF codebook (not machine readable) describing factor values. This function 
+#' factors the raw numeric CPS data according to a supplied factor key.
+#' 
+#' @param data
+#' 
+#' @return
+#' @export
+factor_cps <- function(data, factors = cps_factors) {
+  
+}
