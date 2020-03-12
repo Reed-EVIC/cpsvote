2772ad06bb14fab1455ab584fc1283c807c49eeb
jayleetx <jay.lee.tx@gmail.com>
2020-03-12 16:19:44 -0700

    Trim white space

--- data-raw/save_colspecs.R
+++ data-raw/save_colspecs.R
@@ -6,6 +6,8 @@ cps_factors <- read.csv(here::here('data-raw', 'cps_factors.csv'),
                         na.strings = c("NA", ""),
                         stringsAsFactors = FALSE)
 
+cps_factors$value <- trimws(cps_factors$value)
+
 usethis::use_data(cps_cols, overwrite = TRUE)
 
 usethis::use_data(cps_factors, overwrite = TRUE)
