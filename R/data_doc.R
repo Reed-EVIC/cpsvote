ae49a26f51e2f40ba8c82f9421fb73b486a11008
jayleetx <jay.lee.tx@gmail.com>
2020-03-17 12:38:45 -0700

    Updating documentation

--- R/data_doc.R
+++ R/data_doc.R
@@ -17,3 +17,41 @@
 #' }
 #' @source Turnout data from \url{http://www.electproject.org/home/voter-turnout/voter-turnout-data}
 "reweight"
+
+#' Sample column specifications for reading CPS data
+#' 
+#' Because the CPS is a fixed-width file that changes data locations (and variable 
+#' names) across years, to correctly read the data you have to specify which 
+#' start/end positions correspond to which column names in each year. This is one 
+#' such specification. To add extra data or change column names, see the Vignette.
+#' @format A data frame with 204 rows and 8 columns:
+#' \describe{
+#' \item{year}{year}
+#' \item{cps_name}{original column name as given by the CPS}
+#' \item{new_name}{a new name, which tries to describe the variable and join 
+#' sensibly across multiple years}
+#' \item{start_pos}{which character of a line the variable starts with}
+#' \item{end_pos}{which character of a line the variable ends with}
+#' \item{col_type}{whether the column is character, numeric, or a factor}
+#' \item{description}{the question text/description from the CPS}
+#' \item{notes}{any notes for question administration or analysis}
+#' }
+"cps_cols"
+
+#' Sample factor specifications for reading CPS data
+#' 
+#' Because the CPS changes factor levels across years, to correctly read the data 
+#' you have to specify which numeric codes correspond to which character values 
+#' in each year. This is one such specification. To add extra data, see the Vignette.
+#' @format A data frame with 204 rows and 8 columns:
+#' \describe{
+#' \item{year}{year}
+#' \item{cps_name}{original column name as given by the CPS}
+#' \item{new_name}{a new name, which tries to describe the variable and join 
+#' sensibly across multiple years}
+#' \item{code}{the numeric code contained in the raw CPS data}
+#' \item{value}{the character value corresponding to each numeric code}
+#' }
+#' @details These match the exact specifications from the CPS, including NA codes 
+#' and any typos that occur (e.g., "Hipsanic" is common in older years).
+"cps_factors"
