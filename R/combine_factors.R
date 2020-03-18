417e790684b2ca0226f116bcea821fcfe9a8bdba
jayleetx <jay.lee.tx@gmail.com>
2020-03-17 17:39:19 -0700

    Set factor combination in stone

--- R/combine_factors.R
+++ R/combine_factors.R
@@ -1,17 +1,28 @@
-combine_factors <- function(data, na.vals = c("-1", "BLANK", "NOT IN UNIVERSE", "NO RESPONSE (N/A)", "NO RESPONSE")) {
+#' combine factor levels across years
+#' 
+#' The response sets in certain CPS questions change between years. This function 
+#' consolidates several of these response sets across years (and fixes typos 
+#' from the CPS documentation), specifically race, Hispanic status, duration of 
+#' residency, reason for not voting, and method of registration.
+#' 
+#' @details While consolidating response sets across multiple surveys can be 
+#' fraught with peril, this function attempts to combine disparate levels for 
+#' race and other CPS variable across multiple years. Some of these are 
+#' relatively straightforward typos fixes ("NON-HIPSANIC" should clearly match 
+#' "NON-HISPANIC"), but others have differing degrees of subjectivity applied.
+#' Take this function with a grain of salt, as it depends on some exact variable 
+#' names you may or may not be using, and recode variables as needed for your 
+#' own uses. To explore exactly how these variables were recoded, you can run 
+#' `table(data$RACE, combine_factors(data)$RACE)` in the console, substituting 
+#' your column of interest in for `RACE`.
+#' @param data A dataset containing already-labelled CPS data
+#' @param move_levels Whether to move the levels "OTHER", "DON'T KNOW", and 
+#' "REFUSED" to the end of each factor's level set
+#' @export
+combine_factors <- function(data, move_levels = TRUE) {
   output <- data %>%
-    dplyr::mutate(FILE = factor(FILE),
-                  YEAR = YEAR %% 1900 + 1900,
-                  STATE = factor(STATE, 
-                                 levels = get_levels("STATE")),
-                  AGE = na_ifin(AGE, na.vals),
-                  SEX = factor(toupper(SEX), 
-                               levels = sort(get_levels("SEX"))),
-                  EDUCATION = factor(toupper(EDUCATION), 
-                                     levels = get_levels("EDUCATION")),
-                  RACE = factor(toupper(RACE), 
-                                levels = get_levels("RACE")) %>%
-                    forcats::fct_collapse("WHITE" = c("WHITE", 
+    dplyr::mutate(RACE = forcats::fct_collapse(RACE, # try to consolidate RACE
+                                               "WHITE" = c("WHITE", 
                                                       "WHITE ONLY"),
                                           "BLACK" = c("BLACK", 
                                                       "BLACK ONLY"),
@@ -21,25 +32,14 @@ combine_factors <- function(data, na.vals = c("-1", "BLANK", "NOT IN UNIVERSE", 
                                           "AMERICAN INDIAN OR ALASKA NATIVE" = c("AMERICAN INDIAN, ALEUT, ESKIMO",
                                                                                  "AMERICAN INDIAN, ALASKAN NATIVE ONLY"),
                                           other_level = "TWO OR MORE RACES"),
-                  HISPANIC = factor(toupper(HISPANIC),
-                                    levels = c("HISPANIC", "NON-HISPANIC", "NON-HIPSANIC"),
-                                    labels = c("HISPANIC", "NON-HISPANIC", "NON-HISPANIC")),
-                  WEIGHT = na_ifin(WEIGHT, na.vals) / 10000,
-                  VRS_VOTE = factor(toupper(VRS_VOTE), 
-                                    levels = get_levels("VRS_VOTE")),
-                  VRS_REG = factor(toupper(VRS_REG), 
-                                   levels = get_levels("VRS_REG")),
-                  VRS_VOTE_TIME = factor(toupper(VRS_VOTE_TIME), 
-                                         levels = get_levels("VRS_VOTE_TIME")),
-                  VRS_RESIDENCE = factor(toupper(VRS_RESIDENCE), 
-                                         levels = get_levels("VRS_RESIDENCE")) %>%
-                    forcats::fct_collapse("LESS THAN 1 YEAR" = c("LESS THAN 1 MONTH", # consolidate old/new versions
+                  HISPANIC = forcats::fct_collapse(HISPANIC, "NON-HISPANIC" = "NON-HIPSANIC"), # fix typo
+                  VRS_RESIDENCE = forcats::fct_collapse(VRS_RESIDENCE,
+                    "LESS THAN 1 YEAR" = c("LESS THAN 1 MONTH", # consolidate old/new versions
                                                                  "1-6 MONTHS",
                                                                  "7-11 MONTHS")) %>%
                     forcats::fct_relevel("LESS THAN 1 YEAR"), # stick that one in front
-                  VRS_VOTE_WHYNOT = factor(toupper(VRS_VOTE_WHYNOT), 
-                                           levels = get_levels("VRS_VOTE_WHYNOT")) %>%
-                    forcats::fct_collapse("TRANSPORTATION PROBLEMS" = c("HAD NO WAY TO GET TO POLLS",
+                  VRS_VOTE_WHYNOT = forcats::fct_collapse(VRS_VOTE_WHYNOT,
+                    "TRANSPORTATION PROBLEMS" = c("HAD NO WAY TO GET TO POLLS",
                                                                         "TRANSPORTATION PROBLEMS"),
                                           "SCHEDULE PROBLEMS" = c("COULD NOT TAKE TIME OFF FROM WORK/SCHOOL/TOO BUSY",
                                                                   "TOO BUSY, CONFLICTING WORK OR SCHOOL SCHEDULE"),
@@ -62,15 +62,8 @@ combine_factors <- function(data, na.vals = c("-1", "BLANK", "NOT IN UNIVERSE", 
                                           "OTHER" = c("OTHER REASONS"),
                                           "DON'T KNOW" = c("DON'T KNOW"),
                                           ),
-                  VRS_VOTE_METHOD = factor(toupper(VRS_VOTE_METHOD), 
-                                           levels = get_levels("VRS_VOTE_METHOD")),
-                  VRS_REG_SINCE95 = factor(toupper(VRS_REG_SINCE95), 
-                                           levels = get_levels("VRS_REG_SINCE95")),
-                  VRS_REG_DMV = factor(toupper(VRS_REG_DMV), 
-                                           levels = get_levels("VRS_REG_DMV")),
-                  VRS_REG_METHOD = factor(toupper(VRS_REG_METHOD), 
-                                           levels = get_levels("VRS_REG_METHOD")) %>%
-                    forcats::fct_collapse("PUBLIC ASSISTANCE AGENCY" = c("AT A PUBLIC ASSISTANCE AGENCY (FOR EXAMPLE, MEDICAID, AFDC, OR FOOD STAMP UNEMPLOYMENT OFFICE, OFFICE SERVING DISABLED PERSONS)",
+                  VRS_REG_METHOD = forcats::fct_collapse(VRS_REG_METHOD,
+                    "PUBLIC ASSISTANCE AGENCY" = c("AT A PUBLIC ASSISTANCE AGENCY (FOR EXAMPLE, MEDICAID, AFDC, OR FOOD STAMP UNEMPLOYMENT OFFICE, OFFICE SERVING DISABLED PERSONS)",
                                                                          "AT A PUBLIC ASSISTANCE AGENCY (FOR EXAMPLE, MEDICAID, AFDC, OR FOOD STAMPS OFFICE, AN OFFICE SERVING DISABLED PERSONS, OR AN UNEMPLOYMENT OFFICE)",
                                                                          "AT A PUBLIC ASSISTANCE AGENCY (FOR EXAMPLE, A MEDICAID, AFDC, OR FOOD STAMPS OFFICE, AN OFFICE SERVING DISABLED PERSONS, OR AN UNEMPLOYMENT OFFICE",
                                                                          "AT A PUBLIC ASSISTANCE AGENCY (FOR EXAMPLE, A MEDICAID, AFDC, OR FOOD STAMPS OFFICE, AN OFFICE SERVING DISABLED PERSONS, OR AN UNEMPLOYMENT OFFICE)"),
@@ -87,38 +80,28 @@ combine_factors <- function(data, na.vals = c("-1", "BLANK", "NOT IN UNIVERSE", 
                                           "DMV" = c("AT A DEPARTMENT OF MOTOR VEHICLES (FOR EXAMPLE, WHEN OBTAINING A DRIVER'S LICENSE OR OTHER IDENTIFICATION CARD)"),
                                           "ONLINE" = c("REGISTERED USING THE INTERNET OR ONLINE"),
                                           "OTHER" = c("OTHER PLACE/WAY")
-                                          ),
-                  VRS_REG_WHYNOT = factor(toupper(VRS_REG_WHYNOT), 
-                                           levels = get_levels("VRS_REG_WHYNOT")),
-                  VRS_VOTE_MAIL = factor(toupper(VRS_VOTE_MAIL), 
-                                           levels = get_levels("VRS_VOTE_MAIL")),
-                  VRS_VOTE_DAY = factor(toupper(VRS_VOTE_DAY), 
-                                           levels = get_levels("VRS_VOTE_DAY"))
-           ) %>%
-    dplyr::mutate_if(function(y) !is.numeric(y), function(x) forcats::fct_drop(na_ifin(x, toupper(na.vals))))
+                                          )
+           )
+  
+  if (move_levels) output <- output %>%
+    dplyr::mutate_if(is.factor, function(x) suppressWarnings(forcats::fct_relevel(x, 
+                                                                                  "OTHER", 
+                                                                                  "DON'T KNOW",
+                                                                                  "REFUSED",
+                                                                                  after = Inf)))
   
   output
 }
 
-# helpers for the above
-na_ifin <- function(x, y) {
-  x[x %in% y] <- NA
-  x
-}
-
-get_levels <- function(x, dataset = cps_factors, col = "new_name") {
-  unique(toupper(dataset$value[dataset[[col]] == x]))
-}
-
 # small visual tests
-check_levels <- function(var) {
-  table(data[[var]], output[[var]]) %>%
-    data.frame(stringsAsFactors = FALSE) %>%
-    tidyr::pivot_wider(names_from = "Var2", values_from = "Freq") %>%
-    dplyr::select(-Var1, Var1) %>%
-    dplyr::arrange_if(is.numeric, dplyr::desc)
-}
-
-check_years <- function(var) {
-  table(data[[var]], data$YEAR)
-}
+# check_levels <- function(var) {
+#   table(data[[var]], output[[var]]) %>%
+#     data.frame(stringsAsFactors = FALSE) %>%
+#     tidyr::pivot_wider(names_from = "Var2", values_from = "Freq") %>%
+#     dplyr::select(-Var1, Var1) %>%
+#     dplyr::arrange_if(is.numeric, dplyr::desc)
+# }
+# 
+# check_years <- function(var) {
+#   table(data[[var]], data$YEAR)
+# }
