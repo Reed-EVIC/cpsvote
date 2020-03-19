c2ffdfd3ef64e35f782ddd9c528a454e9bc6f24c
jayleetx <jay.lee.tx@gmail.com>
2020-03-18 18:51:50 -0700

    Add turnout reweight fxn

--- R/cps_load_basic.R
+++ R/cps_load_basic.R
@@ -5,7 +5,8 @@
 #' made more sense to ship a "basic" CPS data set as a function rather than as a 
 #' package data object (which would have been over 10 MB). This function will 
 #' take you from nothing to having some basic CPS data in your environment, with 
-#' the option to save this data locally for future ease.
+#' the option to save this data locally for future ease. A sample of the data
+#' that comes out of this function is provided as `cpsvote::cps_sample_10k`.
 #' 
 #' @param datadir The location where the CPS zip files live (or should be 
 #' downloaded to)
@@ -16,7 +17,8 @@ cps_load_basic <- function(datadir = "cps_data",
   output <- cps_read(dir = datadir) %>%
     cps_label() %>%
     cps_refactor() %>%
-    cps_recode_vote()
+    cps_recode_vote() %>%
+    cps_reweight_turnout()
   
   if(!is.null(outdir)) {
     saveRDS(output, file = file.path(outdir, "cps_basic.RData"))
---
+++ R/cps_reweight_turnout.R
@@ -0,0 +1,29 @@
+#' apply weight correction for voter turnout
+#' 
+#' This function applies the turnout correction recommended by Achen & Hur 
+#' (2013). The data set containing the scaling factor is `cpsvote::cps_reweight`.
+#' @param data the input data set, containing columns `YEAR`, `STATE`, and 
+#' `achenhur_turnout`
+#' 
+#' @export
+cps_reweight_turnout <- function(data) {
+  YEAR <- STATE <- WEIGHT <- response <- NULL
+  
+  reweight <- cpsvote::cps_reweight %>%
+    dplyr::select(YEAR, STATE, response, reweight) %>%
+    dplyr::mutate(YEAR = as.integer(YEAR))
+  output <- data %>%
+    dplyr::left_join(reweight, by = c("YEAR", "STATE", "achenhur_turnout" = "response")) %>%
+    dplyr::mutate(reweight = dplyr::coalesce(reweight, 1), # if it's missing, just return the original
+                  turnout_weight = WEIGHT * reweight) %>%
+    dplyr::select(-reweight)
+  
+  output
+}
+
+# out <- output %>%
+#   srvyr::as_survey_design(weights = turnout_weight)
+# 
+# turnout_check <- out %>%
+#   dplyr::group_by(YEAR, STATE) %>%
+#   dplyr::summarize(turnout = srvyr::survey_mean(achenhur_turnout == "YES", na.rm = TRUE))
