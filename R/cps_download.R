ea10b7bf9479fab3ecd1a53a1435079cc0a7bcdf
John Alexander Curiel <jcuriel.unc@gmail.com>
2026-05-19 10:28:35 -0700

    added in the 2022 data and supporting features
    Extended the relevant factors and reweight data to incorporate the 2022 results. All ought to be good now for purpose of employing the 2022 data.

--- R/cps_download.R
+++ R/cps_download.R
@@ -2,11 +2,11 @@
 #' 
 #' @param path A file path (relative or absolute) where the downloads should go.
 #' @param years Which years of documentation to download. Defaults to all 
-#' even-numbered years from 1994 to 2020.
+#' even-numbered years from 1994 to 2022.
 #' @param overwrite Logical, whether to write over existing files or not. 
 #' Defaults to FALSE.
 #' @details 
-#' * File names will be written in the style "cps_nov2020.pdf", with the 
+#' * File names will be written in the style "cps_nov2022.pdf", with the 
 #' appropriate years.
 #' * The Voting and Registration Supplement is only conducted in even-numbered 
 #' years (since 1964), so any entry in `years` outside of this will be skipped.
@@ -19,7 +19,7 @@
 #' 
 #' @export
 cps_download_docs <- function(path = "cps_docs", 
-                          years = seq(1994, 2020, 2),
+                          years = seq(1994, 2022, 2),
                           overwrite = FALSE) {
   
   # sanitize inputs #####
@@ -39,24 +39,24 @@ cps_download_docs <- function(path = "cps_docs",
     years <- years[years >= 1994]
   }
   
-  # years must be before 2020
-  if (any(years > 2020)) {
-    warning(paste0("The Census Bureau has not yet released CPS data for years after 2020. The remaining years listed (",
-                   paste(years[years <= 2020], collapse = ", "),
+  # years must be before 2022
+  if (any(years > 2022)) {
+    warning(paste0("The Census Bureau has not yet released CPS data for years after 2022. The remaining years listed (",
+                   paste(years[years <= 2022], collapse = ", "),
                    ") will be downloaded."),
             immediate. = T)
-    years <- years[years <= 2020]
+    years <- years[years <= 2022]
   }
   
   # years must be in survey coverage zone
-  if (!all(years %in% seq(1964, 2020, 2))) {
+  if (!all(years %in% seq(1964, 2022, 2))) {
     warning(paste0("The VRS was not conducted in the following years: ",
-                   paste(years[!(years %in% seq(1964, 2020, 2))], collapse = ", "),
+                   paste(years[!(years %in% seq(1964, 2022, 2))], collapse = ", "),
                    ". The remaining years listed (",
-                   paste(years[years %in% seq(1964, 2020, 2)], collapse = ", "),
+                   paste(years[years %in% seq(1964, 2022, 2)], collapse = ", "),
                    ") will be downloaded."),
             immediate. = T)
-    years <- years[years %in% seq(1964, 2020, 2)]
+    years <- years[years %in% seq(1964, 2022, 2)]
   }
   
   # overwrite must be T/F
@@ -117,11 +117,11 @@ cps_download_docs <- function(path = "cps_docs",
 #' 
 #' @param path A file path (relative or absolute) where the downloads should go.
 #' @param years Which years of data to download. Defaults to all 
-#' even-numbered years from 1994 to 2020.
+#' even-numbered years from 1994 to 2022.
 #' @param overwrite Logical, whether to write over existing files or not. 
 #' Defaults to FALSE.
 #' @details 
-#' * File names will be written in the style "cps_nov2020.zip", with the 
+#' * File names will be written in the style "cps_nov2022.zip", with the 
 #' appropriate years.
 #' * The Voting and Registration Supplement is only conducted in even-numbered 
 #' years (since 1964), so any entry in `years` outside of this will be skipped.
@@ -134,7 +134,7 @@ cps_download_docs <- function(path = "cps_docs",
 #' 
 #' @export
 cps_download_data <- function(path = "cps_data", 
-                          years = seq(1994, 2020, 2), 
+                          years = seq(1994, 2022, 2), 
                           overwrite = FALSE) {
   
   # sanitize inputs #####
@@ -154,24 +154,24 @@ cps_download_data <- function(path = "cps_data",
     years <- years[years >= 1994]
   }
   
-  # years must be before 2020
-  if (any(years > 2020)) {
-    warning(paste0("The Census Bureau has not yet released CPS data for years after 2020. The remaining years listed (",
-                   paste(years[years <= 2020], collapse = ", "),
+  # years must be before 2022
+  if (any(years > 2022)) {
+    warning(paste0("The Census Bureau has not yet released CPS data for years after 2022. The remaining years listed (",
+                   paste(years[years <= 2022], collapse = ", "),
                    ") will be downloaded."),
             immediate. = T)
-    years <- years[years <= 2020]
+    years <- years[years <= 2022]
   }
   
   # years must be in survey coverage zone
-  if (!all(years %in% seq(1964, 2020, 2))) {
+  if (!all(years %in% seq(1964, 2022, 2))) {
     warning(paste0("The VRS was not conducted in the following years: ",
-                   paste(years[!(years %in% seq(1964, 2020, 2))], collapse = ", "),
+                   paste(years[!(years %in% seq(1964, 2022, 2))], collapse = ", "),
                    ". The remaining years listed (",
-                   paste(years[years %in% seq(1964, 2020, 2)], collapse = ", "),
+                   paste(years[years %in% seq(1964, 2022, 2)], collapse = ", "),
                    ") will be downloaded."),
             immediate. = T)
-    years <- years[years %in% seq(1964, 2020, 2)]
+    years <- years[years %in% seq(1964, 2022, 2)]
   }
   
   # overwrite must be T/F
