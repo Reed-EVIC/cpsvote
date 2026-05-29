c0b944cd087c6a5b7d5a31c8a700b59053739c4e
Paul Gronke <paulgronke@gmail.com>
2026-05-28 23:37:52 -0700

    Create a new snowglobe!

--- sandbox/hisp_plot.R
+++ sandbox/hisp_plot.R
@@ -1,15 +1,20 @@
-cps_basic <- cps_load_basic(datadir = here::here('cps_data')) 
-cps_basic_W <- as_survey_design(cps_basic, weights = turnout_weight)
+# Creates a plot of Turnout by Hispanic and Non Hispanic across age groups
+
+library(tidyverse)
+library(survey)
+library(srvyr)
+
+cps_basic_W <- as_survey_design(cps, weights = turnout_weight)
 
 age_data <- cps_basic_W %>%
-  filter(!is.na(HISPANIC) & !is.na(AGE) & YEAR %in% c(2016, 2018)) %>%
+  filter(!is.na(HISPANIC) & !is.na(AGE) & YEAR %in% c(2016, 2022)) %>%
   mutate(AGE = factor(AGE)) %>%
   group_by(HISPANIC, AGE) %>%
   summarize(value = survey_mean(na.rm = T)) %>%
   mutate(AGE= as.numeric(AGE)) 
 
-cps_basic %>%
-  filter(!is.na(HISPANIC) & !is.na(AGE) & YEAR %in% c(2016, 2018)) %>%
+cps %>%
+  filter(!is.na(HISPANIC) & !is.na(AGE) & YEAR %in% c(2016, 2022)) %>%
   group_by(AGE) %>%
   summarise(n())
 
